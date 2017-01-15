b164e83e94e0dc6e493cd2ff3a06a8421a51a76a
rajendraprasad-j <rajendraprasad.jee@gmail.com>
2017-01-15 16:14:22 +0530

    Bug

--- services/sendTextMessage.js
+++ services/sendTextMessage.js
@@ -1,5 +1,5 @@
 var callSendAPI=require("./api.ai")
 
 module.exports=function sendTextMessage(recipientId, messageText) {
-  callSendAPI(recipientId,messageText);
+  callSendAPI(messageText,recipientId);
 }
