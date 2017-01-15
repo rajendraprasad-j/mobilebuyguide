51af6e341e850777096a2fe121cab0dc65f7a2c0
rajendraprasad-j <rajendraprasad.jee@gmail.com>
2017-01-15 14:30:19 +0530

    Test

--- routes/routes.js
+++ routes/routes.js
@@ -15,9 +15,7 @@ module.exports=function(app){
     }
   });
   app.post('/webhook', jsonParser,function (req, res) {
-    console.log(req);
     var data = req.body;
-
     // Make sure this is a page subscription
     if (data.object === 'page') {
 
@@ -46,7 +44,37 @@ module.exports=function(app){
   });
 
   function receivedMessage(event) {
-    // Putting a stub for now, we'll expand it in the following steps
-    console.log("Message data: ", event.message);
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+      senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+      // If we receive a text message, check to see if it matches a keyword
+      // and send back the example. Otherwise, just echo the text we received.
+      switch (messageText) {
+        case 'generic':
+          sendGenericMessage(senderID);
+          break;
+
+        default:
+          sendTextMessage(senderID, messageText);
+      }
+    } else if (messageAttachments) {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
+  }
+  function sendGenericMessage(recipientId, messageText) {
+    // To be expanded in later sections
   }
 }
