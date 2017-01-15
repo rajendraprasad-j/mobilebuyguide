b686aab01acae35607b91db89be6ecadd48aad70
rajendraprasad-j <rajendraprasad.jee@gmail.com>
2017-01-15 16:07:11 +0530

    Call api.a

---
+++ services/api.ai.js
@@ -0,0 +1,49 @@
+var apiai = require('apiai');
+var PAGE_ACCESS_TOKEN="EAAJ94KKZCT1IBAMWoxJApEeFwa6YjpEMQ5v4IPbeyKPcPdjMeZBDUqfEAE5isxj9xPb6ErpEaK00qEN7LRDzV3qQITldIOZCb4t6FKQdYncD6a2gfCYhZAVLSmaE48UJ5CgssU4UdcJpbBVZBCA8jGYAhCyGkZCtjzS2BYDZC5xLQZDZD";
+var app = apiai("dbdf3c0e3afb40b0a2d1fbb805d0976c");
+module.exports=function(requestText,sessionId){
+  app.textRequest(requestText, {
+      sessionId: sessionId
+  });
+
+  request.on('response', function(response) {
+      console.log(response);
+      var messageData = {
+        recipient: {
+          id: sessionId
+        },
+        message: {
+          text: response
+        }
+      };
+      callSendAPI(messageData);
+  });
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+  request.end();
+
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
--- services/sendTextMessage.js
+++ services/sendTextMessage.js
@@ -1,36 +1,6 @@
 var request = require('request');
-var PAGE_ACCESS_TOKEN="EAAJ94KKZCT1IBAMWoxJApEeFwa6YjpEMQ5v4IPbeyKPcPdjMeZBDUqfEAE5isxj9xPb6ErpEaK00qEN7LRDzV3qQITldIOZCb4t6FKQdYncD6a2gfCYhZAVLSmaE48UJ5CgssU4UdcJpbBVZBCA8jGYAhCyGkZCtjzS2BYDZC5xLQZDZD";
+var callSendAPI=require("./api.ai")
+
 module.exports=function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });
+  callSendAPI(recipientId,messageText);
 }
